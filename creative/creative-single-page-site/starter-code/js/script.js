8bed91db59ade9a54c172359410960c4bb28d8db
Martin Ondráš <numenor.ondras@gmail.com>
2021-08-20 14:35:55 +0200

    resending the correct creative site

--- creative/creative-single-page-site/starter-code/js/script.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
-
-burger.addEventListener("click", function(e) {
-    nav.classList.toggle("open");
-});
